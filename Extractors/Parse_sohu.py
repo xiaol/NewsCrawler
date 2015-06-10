334400ce8e6a008fd881f02716cddec1498b6511
张志广 <zhange.zzg@gmail.com>
2015-06-10 12:14:13 +0800

    Create extractor of sohu news.

--- Extractors/Parse_sohu.py
+++ Extractors/Parse_sohu.py
@@ -6,3 +6,129 @@
     Author:  ZG
     Date:    15/6/9
 """
+
+from Dates.Dates import Dates
+from Extractor import Extractor
+from collections import defaultdict
+
+
+class Parser(object):
+
+    @classmethod
+    def get_title(cls, root):
+        attrs = [{'attr': 'class', 'value': 'finTit'}]
+        tag = Extractor.get_ment_by_attrs(root, attrs)
+        if tag is not None:
+            tag = tag.text.strip()
+            return tag
+        else:
+            return None
+
+    @classmethod
+    def get_tag(cls, root):
+        tags = []
+        attrs = [{'attr': 'name', 'value': 'keywords'}]
+        tag = Extractor.get_ment_by_attrs(root, attrs)
+        if tag is not None:
+            tags = tag.get('content')
+            tags = tags.replace(',${after}', '').strip()
+            tags = ','.join(tags.split())
+        return tags or None
+
+    @classmethod
+    def get_origin(cls, root):
+        attrs = [{'attr': 'class', 'value': 'source'}]
+        origin = Extractor.get_ment_by_attrs(root, attrs)
+        if origin is not None:
+            origin = origin.text_content()
+            origin = origin.split()[0]
+            return origin
+        else:
+            return None
+
+    @classmethod
+    def get_originurl(cls, root):
+        return
+
+    @classmethod
+    def get_author(cls, root):
+        return
+
+    @classmethod
+    def get_date(cls, root):
+        attrs = [{'attr': 'class', 'value': 'inf'},
+                 ]
+        date = Extractor.get_ment_by_attrs(root, attrs)
+        if date is not None:
+            date = date.text_content()
+            date = date.strip().split()[:-1]
+            date = ' '.join(date)
+            return date
+        else:
+            return Dates.time()
+
+    @classmethod
+    def get_imgnum(cls, cont):
+        items = [item.values() for item in cont]
+        img = [item for item in items if item[0].keys()[0] == 'img']
+        return len(img)
+
+    @classmethod
+    def get_content(cls, root):
+        picli = []
+
+        attrs = [{'attr': 'class', 'value': 'finCnt'},
+                 ]
+        conts = Extractor.get_ment_by_attrs(root, attrs)
+        # print 'conts:', conts
+        if conts:
+            picli += cls.cont_format(conts)
+        return picli
+
+    @staticmethod
+    def cont_format(node):
+        contents = []
+        dedupe = set()
+
+        for child in node.iter():
+            item = defaultdict(dict)
+
+            print 'Tag', child.tag, 'ATTR', child.attrib
+
+            # tags filter
+            if 'class' in child.attrib and child.attrib['class'] == 'finCnt':
+                continue
+            if 'class' in child.attrib and child.attrib['class'] == 'toShare clearfix':
+                break
+
+            # video frame
+            if 'class' in child.attrib and child.attrib['class'] == 'finVideo':
+                continue
+
+            # image frame
+            if child.tag == 'img':
+                img = child.get('src') or child.get('alt_src')
+                if img and img not in dedupe:
+                    item[str(len(contents))]['img'] = img
+                    contents.append(item)
+                    dedupe.add(img)
+                item = defaultdict(dict)
+                txt = child.get('alt')
+                txt = txt.strip() if txt else None
+                if txt and txt not in dedupe:
+                    item[str(len(contents))]['img_info'] = txt
+                    contents.append(item)
+                    dedupe.add(txt)
+                continue
+
+            # news content
+            if child.tag == 'p':
+                txt = child.text_content()
+                txt = txt.strip() if txt else None
+                if txt and txt not in dedupe:
+                    item[str(len(contents))]['txt'] = txt
+                    contents.append(item)
+                    dedupe.add(txt)
+                continue
+
+        return contents or []
