e8ad5f9b0462033cc174b2ceb973df859244ea0f
张志广 <zhange.zzg@gmail.com>
2015-06-10 18:27:50 +0800

    Add the model of pop messages from redis.

--- Reqs/Popqueue.py
+++ Reqs/Popqueue.py
@@ -6,3 +6,24 @@
     Author:  ZG
     Date:    15/6/8
 """
+
+import time
+
+
+class Popqueue(object):
+
+    @classmethod
+    def rpop(cls, r, qname):
+        time.sleep(3)
+        # pop url from setup url list from redis
+        while 1:
+            # set_up:url
+            l = r.llen(qname)
+            if l == 0:
+                print 'Queue-->%s-->Wait' % qname
+                time.sleep(5)
+                return 'http://'
+            rpop = r.rpop(qname)
+            print 'Queue-->%s-->%s' % (qname, l)
+            print 'Queue-->%s-->%s' % (qname, rpop)
+            return rpop
