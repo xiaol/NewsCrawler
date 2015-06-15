c8ffda6546f0b71a5434d0a067016b9c50ec4caa
张志广 <zhange.zzg@gmail.com>
2015-06-15 10:56:19 +0800

    Add script to get the spider status.

---
+++ Controller/spider_status.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+from scrapyd_api import ScrapydAPI
+
+print '.'*60
+scrapyd = ScrapydAPI('http://localhost:6800')
+list_projects = scrapyd.list_projects()
+print list_projects
+
+for project in list_projects:
+    if project == 'default':
+        continue
+    print '.'*60
+    list_spiders = scrapyd.list_spiders(project)
+    print 'project:', project
+    for spider in list_spiders:
+        print 'spider:', spider
+    status = scrapyd.list_jobs(project)
+    running_spider = status['running']
+    print 'running spider:'
+    for spider in running_spider:
+        print spider
+
+
