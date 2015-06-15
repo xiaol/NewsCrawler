8da805400682df718894ec9ef18f05c6d98c17e4
张志广 <zhange.zzg@gmail.com>
2015-06-15 10:55:50 +0800

    Add script to run spiders.

---
+++ Controller/run_spider.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+
+from scrapyd_api import ScrapydAPI
+
+scrapyd = ScrapydAPI('http://localhost:6800')
+list_projects = scrapyd.list_projects()
+print list_projects
+
+for project in list_projects:
+    if project == 'default':
+        continue
+    print '.'*60
+    list_spiders = scrapyd.list_spiders(project)
+    print 'project:', project
+    for spider in list_spiders:
+        print 'spider:', spider
+    status = scrapyd.list_jobs(project)
+    running_jogs = status['running']
+    running_spiders = [running_jog['spider'] for running_jog in running_jogs]
+    for spider_name in list_spiders:
+        if spider_name not in running_spiders:
+             job_id = scrapyd.schedule(project, spider_name)
+        else:
+            print '%s is running.' % spider_name
+
