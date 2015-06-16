d76e5cc297510481111d2f315742e33097f77c88
张志广 <zhange.zzg@gmail.com>
2015-06-16 16:29:30 +0800

    Add content spider of fenghaung important news.

---
+++ NewsCrawler/spiders/ContentSpiderFenghuangImportant.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+"""
+    Project:
+    Purpose: 
+    Version:
+    Author:  ZG
+    Date:    15/6/16
+"""
+
+import sys
+import redis
+from scrapy.spider import Spider
+from lxml.html import soupparser as soup
+from Cleaners.Cleaners import Cleaners
+from Extractors.Parse_ifeng import Parser
+from NewsCrawler.items import ContItem
+from Reqs.Popqueue import Popqueue
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+
+class ContentSpiderFenghuangImportant(Spider):
+
+    name = 'content_spider_fenghuang_important'
+    qname = 'fenghuang_important:content'
+    pool = redis.ConnectionPool(host='localhost', port=6379, db=0)
+    r = redis.Redis(connection_pool=pool)
+
+    # start_urls = [
+    #     'http://news.ifeng.com/a/20150616/43983518_0.shtml',
+    # ]
+
+    def start_requests(self):
+        # formate start_urls from redis pop
+        while 1:
+            yield self.make_requests_from_url(
+                Popqueue.rpop(self.r, self.qname)
+            )
+
+    def parse(self, response):
+        item = ContItem()
+
+        source = response.body
+        root = soup.fromstring(source)
+
+        item['url'] = response.url
+        item['title'] = Parser.get_title(root) or ''
+        item['tags'] = Parser.get_tag(root) or ''
+        item['source'] = 'ifeng'
+        item['source_url'] = response.url
+        item['author'] = Parser.get_author(root) or ''
+        item['update_time'] = Parser.get_date(root) or ''
+        item['content'] = Parser.get_content(item['url'], root) or []
+        item['imgnum'] = Parser.get_imgnum(item['content']) if item['content'] else 0
+
+        yield item
