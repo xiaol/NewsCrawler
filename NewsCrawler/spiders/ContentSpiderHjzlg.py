6cdad3665c26ecff3929a87c05ed7e3e0460b2c5
张志广 <zhange.zzg@gmail.com>
2015-06-23 11:06:19 +0800

    Add content spider of hjzlg.com.

---
+++ NewsCrawler/spiders/ContentSpiderHjzlg.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+"""
+    Project:
+    Purpose: 
+    Version:
+    Author:  ZG
+    Date:    15/6/16
+"""
+
+import sys
+import redis
+from scrapy.spider import Spider
+from lxml.html import soupparser as soup
+from Cleaners.Cleaners import Cleaners
+from Extractors.Parse_hjzlg import Parser
+from NewsCrawler.items import ContItem
+from Reqs.Popqueue import Popqueue
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+
+class ContentSpiderHjzlg(Spider):
+
+    name = 'content_spider_hjzlg'
+    qname = 'hjzlg:content'
+    pool = redis.ConnectionPool(host='localhost', port=6379, db=0)
+    r = redis.Redis(connection_pool=pool)
+
+    # start_urls = [
+    #     'http://www.hjzlg.com/web5/YCMS_ShowNews.asp?id=62126',
+    # ]
+
+    def start_requests(self):
+        # formate start_urls from redis pop
+        while 1:
+            yield self.make_requests_from_url(
+                Popqueue.rpop(self.r, self.qname)
+            )
+
+    def parse(self, response):
+        item = ContItem()
+
+        source = response.body
+        root = soup.fromstring(source)
+
+        item['url'] = response.url
+        item['title'] = Parser.get_title(root) or ''
+        item['tags'] = ''
+        item['source'] = 'hjzlg.com'
+        item['source_url'] = response.url
+        item['author'] = ''
+        item['update_time'] = ''
+        item['content'] = Parser.get_content(item['url'], root) or []
+        item['imgnum'] = Parser.get_imgnum(item['content']) if item['content'] else 0
+
+        yield item
