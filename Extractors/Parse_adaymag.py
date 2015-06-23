7ac5330c00d25610f08e7707e904e1550dd0f185
张志广 <zhange.zzg@gmail.com>
2015-06-23 14:46:48 +0800

    Add parser of a day magazine.

---
+++ Extractors/Parse_adaymag.py
@@ -0,0 +1,165 @@
+# -*- coding: utf-8 -*-
+"""
+    Project:
+    Purpose: 
+    Version:
+    Author:  ZG
+    Date:    15/6/9
+"""
+
+from Dates.Dates import Dates
+from Extractor import Extractor
+from Reqs.Reqs import Reqs
+from collections import defaultdict
+import simplejson as json
+from lxml.html import soupparser as soup
+
+
+class Parser(object):
+
+    @classmethod
+    def get_title(cls, root):
+        attrs = [{'attr': 'name', 'value': 'entry-title'}]
+        tag = Extractor.get_ment_by_attrs(root, attrs)
+        if tag is not None:
+            tag = tag.text_content()
+            return tag
+        else:
+            return None
+
+    @classmethod
+    def get_tag(cls, root):
+        tags = []
+        attrs = [{'attr': 'name', 'value': 'keywords'}]
+        tag = Extractor.get_ment_by_attrs(root, attrs)
+        if tag is not None:
+            tags = tag.get('content')
+            tags = ','.join(tags.split())
+        return tags or None
+
+    @classmethod
+    def get_origin(cls, root):
+        attrs = [{'attr': 'class', 'value': 'author'}]
+        origin = Extractor.get_ment_by_attrs(root, attrs)
+        if origin is not None:
+            origin = origin.text_content()
+            origin = origin.split()[0]
+            return origin
+        else:
+            return None
+
+    @classmethod
+    def get_originurl(cls, root):
+        return
+
+    @classmethod
+    def get_author(cls, root):
+        attrs = [{'attr': 'class', 'value': 'authorName'}]
+        origin = Extractor.get_ment_by_attrs(root, attrs)
+        if origin is not None:
+            origin = origin.text_content()
+            origin = origin.split()[0]
+            return origin
+        else:
+            return None
+
+    @classmethod
+    def get_date(cls, root):
+        attrs = [{'attr': 'class', 'value': 'entry-date updated'},
+                 ]
+        date = Extractor.get_ment_by_attrs(root, attrs)
+        if date is not None:
+            date = date.text_content()
+            date = date.strip().split()[:-1]
+            date = ' '.join(date)
+            return date
+        else:
+            return Dates.time()
+
+    @classmethod
+    def get_imgnum(cls, cont):
+        items = [item.values() for item in cont]
+        img = [item for item in items if item[0].keys()[0] == 'img']
+        return len(img)
+
+    @classmethod
+    def get_content(cls, url, root):
+        picli = []
+
+        attrs = [
+            # {'attr': 'class', 'value': 'td-paragraph-padding-1'},
+            {'attr': 'class', 'value': 'td-post-text-content'},
+            ]
+        conts = Extractor.get_ment_by_attrs(root, attrs)
+
+        # print 'conts:', conts
+        if conts is not None:
+            picli += cls.cont_format(conts)
+        else:
+            return picli
+
+        return picli
+
+    @staticmethod
+    def cont_format(node):
+        contents = []
+        dedupe = set()
+
+        # Content info with tag is p, delete it.
+        try:
+            node.find('.//div[@class="essb_links  essb_counters essb_displayed_both essb_template_round-retina'
+                      ' essb_959191419 print-no "]').drop_tree()
+        except AttributeError:
+            pass
+
+        for child in node.iter():
+            item = defaultdict(dict)
+
+            print 'Tag', child.tag, 'ATTR', child.attrib
+
+            # tags filter
+            if 'class' in child.attrib and child.attrib['class'] == 'td-paragraph-padding-1':
+                continue
+            if 'class' in child.attrib and child.attrib['class'] == 'line-share':
+                break
+
+            # video frame
+            if 'id' in child.attrib and child.attrib['id'] == 'mod_player':
+                continue
+
+            # image frame
+            if child.tag == 'img':
+                img = child.get('src') or child.get('alt_src')
+                if img and img not in dedupe:
+                    if img.startswith('//'):
+                        img = ''.join(['http:', img])
+                    item[str(len(contents))]['img'] = img
+                    contents.append(item)
+                    dedupe.add(img)
+                continue
+
+            # news content
+            if child.tag == 'p':
+
+                # content
+                txt = child.text_content()
+                txt = txt.strip() if txt else None
+                if txt and txt not in dedupe:
+                    item[str(len(contents))]['txt'] = txt
+                    contents.append(item)
+                    dedupe.add(txt)
+                continue
+
+            # block title
+            if child.tag == 'h4':
+                # content
+                txt = child.text_content()
+                txt = txt.strip() if txt else None
+                if txt and txt not in dedupe:
+                    txt = ' '.join(txt.split('\n'))
+                    item[str(len(contents))]['txt'] = txt
+                    contents.append(item)
+                    dedupe.add(txt)
+                continue
+
+        return contents or []
