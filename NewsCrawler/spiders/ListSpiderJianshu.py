5c125fa411954bc2adfbdba015b883d6e6d53200
张志广 <zhange.zzg@gmail.com>
2015-06-24 11:19:21 +0800

    Add list spider of jianshu.com .

---
+++ NewsCrawler/spiders/ListSpiderJianshu.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+"""
+    Project:
+    Purpose: 
+    Version:
+    Author:  ZG
+    Date:    15/6/16
+"""
+
+import sys
+import redis
+from scrapy.spider import Spider
+from collections import defaultdict
+from lxml.html import soupparser as soup
+from Cleaners.Cleaners import Cleaners
+from Cleaners.Encoding import encode_value
+from Extractors.Extractor import Extractor
+from Reqs.Popqueue import Popqueue
+from NewsCrawler.items import ListItem
+from Cleaners.Encoding import encode_value
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+
+class ListSpiderJianshu(Spider):
+
+    name = 'list_spider_jianshu'
+    qname = 'jianshu'
+    pool = redis.ConnectionPool(host='localhost', port=6379, db=0)
+    r = redis.Redis(connection_pool=pool)
+
+    # 简书：http://www.jianshu.com/
+    # start_urls = [
+    #     'http://www.jianshu.com/',
+    # ]
+
+    def start_requests(self):
+        # formate start_urls from redis pop
+        while 1:
+            yield self.make_requests_from_url(
+                Popqueue.rpop(self.r, self.qname)
+            )
+
+    def parse(self, response):
+        item = ListItem()
+
+        source = response.body
+        root = soup.fromstring(source)
+
+        xp = '//*[@class="article-list thumbnails"]/li/div/h4/a'
+        urls = Extractor.get_list(root, xp)
+        uris_titles = defaultdict(str)
+        for it in urls:
+            url = it[1]
+            title = it[0]
+            if not url.startswith('http'):
+                url = ''.join(['http://www.jianshu.com', url])
+            uris_titles[url] = title
+
+        # for k, v in uris_titles.iteritems():
+        #     print k, v
+
+        item['start_url'] = response.url
+        item['urls'] = uris_titles
+
+        yield item
