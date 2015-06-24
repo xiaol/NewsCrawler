388c3e77049eb34e4ee1cdb2308c9f3f2892f275
张志广 <zhange.zzg@gmail.com>
2015-06-24 14:08:02 +0800

    Add list spider of pento.cn .

---
+++ NewsCrawler/spiders/ListSpiderPento.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+"""
+    Project:
+    Purpose: 
+    Version:
+    Author:  ZG
+    Date:    15/6/16
+"""
+
+import sys
+import redis
+from scrapy.spider import Spider
+from collections import defaultdict
+from lxml.html import soupparser as soup
+from Cleaners.Cleaners import Cleaners
+from Cleaners.Encoding import encode_value
+from Extractors.Extractor import Extractor
+from Reqs.Popqueue import Popqueue
+from NewsCrawler.items import ListItem
+from Cleaners.Encoding import encode_value
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+
+class ListSpiderPento(Spider):
+
+    name = 'list_spider_pento'
+    qname = 'pento'
+    pool = redis.ConnectionPool(host='localhost', port=6379, db=0)
+    r = redis.Redis(connection_pool=pool)
+
+    # 品读：http://www.pento.cn/timeline/recommend/pins/list.html?offset=0&count=20
+    # start_urls = [
+    #     'http://www.pento.cn/timeline/recommend/pins/list.html?offset=0&count=20',
+    # ]
+
+    def start_requests(self):
+        # formate start_urls from redis pop
+        while 1:
+            yield self.make_requests_from_url(
+                Popqueue.rpop(self.r, self.qname)
+            )
+
+    def parse(self, response):
+        item = ListItem()
+
+        source = response.body
+        root = soup.fromstring(source)
+
+        xp = '//*[@class="book_card_title book_big_card_title"]/a'
+        urls = Extractor.get_list(root, xp)
+        uris_titles = defaultdict(str)
+        for it in urls:
+            url = it[1]
+            title = it[0]
+            if not url.startswith('http'):
+                url = ''.join(['http://www.pento.cn', url])
+            uris_titles[url] = title
+
+        # for k, v in uris_titles.iteritems():
+        #     print k, v
+
+        item['start_url'] = response.url
+        item['urls'] = uris_titles
+
+        yield item
