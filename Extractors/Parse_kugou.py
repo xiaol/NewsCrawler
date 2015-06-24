c1f97b4cf8fe75087d7c1a4d1b84680477ab8eee
张志广 <zhange.zzg@gmail.com>
2015-06-24 18:12:30 +0800

    Add parser of kugou.com .

---
+++ Extractors/Parse_kugou.py
@@ -0,0 +1,147 @@
+# -*- coding: utf-8 -*-
+"""
+    Project:
+    Purpose: 
+    Version:
+    Author:  ZG
+    Date:    15/6/9
+"""
+
+from Dates.Dates import Dates
+from Extractor import Extractor
+from Reqs.Reqs import Reqs
+from collections import defaultdict
+import simplejson as json
+from lxml.html import soupparser as soup
+
+
+class Parser(object):
+
+    @classmethod
+    def get_title(cls, root):
+        attrs = [{'attr': 'class', 'value': 'detailTitle'}]
+        tag = Extractor.get_ment_by_attrs(root, attrs)
+        if tag is not None:
+            tag = tag.text_content().strip()
+            return tag
+        else:
+            return None
+
+    @classmethod
+    def get_tag(cls, root):
+        tags = []
+        attrs = [{'attr': 'name', 'value': 'keywords'}]
+        tag = Extractor.get_ment_by_attrs(root, attrs)
+        if tag is not None:
+            tags = tag.get('content')
+            tags = ','.join(tags.split())
+        return tags or None
+
+    @classmethod
+    def get_origin(cls, root):
+        attrs = [{'attr': 'class', 'value': 'source'}]
+        origin = Extractor.get_ment_by_attrs(root, attrs)
+        if origin is not None:
+            origin = origin.text_content()
+            origin = origin.split()[0]
+            return origin
+        else:
+            return None
+
+    @classmethod
+    def get_originurl(cls, root):
+        return
+
+    @classmethod
+    def get_author(cls, root):
+        attrs = [{'attr': 'class', 'value': 'special usercard'}]
+        origin = Extractor.get_ment_by_attrs(root, attrs)
+        if origin is not None:
+            origin = origin.text_content()
+            origin = origin.split()[0]
+            return origin
+        else:
+            return None
+
+    @classmethod
+    def get_date(cls, root):
+        attrs = [{'attr': 'class', 'value': 'article-info'},
+                 ]
+        date = Extractor.get_ment_by_attrs(root, attrs)
+        if date is not None:
+            date = date.text_content()
+            date = date.strip().split()[1:]
+            date = ' '.join(date)
+            return date
+        else:
+            return Dates.time()
+
+    @classmethod
+    def get_imgnum(cls, cont):
+        items = [item.values() for item in cont]
+        img = [item for item in items if item[0].keys()[0] == 'img']
+        return len(img)
+
+    @classmethod
+    def get_content(cls, url, root):
+        picli = []
+
+        attrs = [{'attr': 'class', 'value': 'conttxtclass detailText'},
+                 ]
+        conts = Extractor.get_ment_by_attrs(root, attrs)
+
+        # print 'conts:', conts
+        if conts:
+            picli += cls.cont_format(conts)
+
+        return picli
+
+    @staticmethod
+    def cont_format(node):
+        contents = []
+        dedupe = set()
+
+        # Content info with tag is p, delete it.
+        try:
+            node.find('.//p[@class="time"]').drop_tree()
+        except AttributeError:
+            pass
+        try:
+            node.find('.//div[@id="detail_slide_min"]').drop_tree()
+        except AttributeError:
+            pass
+
+        for child in node.iter():
+            item = defaultdict(dict)
+
+            print 'Tag', child.tag, 'ATTR', child.attrib
+
+            # tags filter
+            if 'class' in child.attrib and child.attrib['class'] == 'mpd-content':
+                continue
+            # if child.tag == 'span':
+            #     continue
+            if 'class' in child.attrib and child.attrib['class'] == 'label':
+                break
+
+            # video frame
+            if 'class' in child.attrib and child.attrib['class'] == 'finVideo':
+                continue
+
+            # image frame
+            if child.tag == 'img':
+                img = child.get('src') or child.get('alt_src')
+                if img and img not in dedupe:
+                    item[str(len(contents))]['img'] = img
+                    contents.append(item)
+                    dedupe.add(img)
+
+            if child.tag == 'p':
+                txt = child.text_content()
+                txt = txt.strip() if txt else None
+                if txt and txt not in dedupe:
+                    item[str(len(contents))]['txt'] = txt
+                    contents.append(item)
+                    dedupe.add(txt)
+
+        return contents or []
