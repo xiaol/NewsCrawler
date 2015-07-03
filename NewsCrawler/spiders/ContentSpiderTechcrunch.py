ed3c5aed7d94db29ad7eaa685095e9dcbe2fa23f
张志广 <zhange.zzg@gmail.com>
2015-07-03 16:03:39 +0800

    Add content spider of techcrunch.cn .

---
+++ NewsCrawler/spiders/ContentSpiderTechcrunch.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+"""
+    Project:
+    Purpose: 
+    Version:
+    Author:  ZG
+    Date:    15/6/16
+"""
+
+import sys
+import redis
+from scrapy.spider import Spider
+from lxml.html import soupparser as soup
+from Cleaners.Cleaners import Cleaners
+from Extractors.Parse_techcrunch import Parser
+from NewsCrawler.items import ContItem
+from Reqs.Popqueue import Popqueue
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+
+class ContentSpiderTechcrunch(Spider):
+
+    name = 'content_spider_techcrunch'
+    qname = 'techcrunch:content'
+    pool = redis.ConnectionPool(host='localhost', port=6379, db=0)
+    r = redis.Redis(connection_pool=pool)
+
+    # start_urls = [
+    #     'http://techcrunch.cn/2015/06/30/the-race-to-the-bottom-is-now-hitting-professional-3d-printers/',
+    # ]
+
+    def start_requests(self):
+        # formate start_urls from redis pop
+        while 1:
+            yield self.make_requests_from_url(
+                Popqueue.rpop(self.r, self.qname)
+            )
+
+    def parse(self, response):
+        item = ContItem()
+
+        source = response.body
+        root = soup.fromstring(source)
+
+        item['url'] = response.url
+        item['title'] = Parser.get_title(root) or ''
+        item['tags'] = ''
+        item['source'] = 'techcrunch.cn'
+        item['source_url'] = response.url
+        item['author'] = ''
+        item['update_time'] = Parser.get_date(root) or ''
+        item['content'] = Parser.get_content(item['url'], root) or []
+        item['imgnum'] = Parser.get_imgnum(item['content']) if item['content'] else 0
+
+        yield item
