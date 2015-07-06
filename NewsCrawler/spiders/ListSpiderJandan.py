daacaa76d0b85534495f2cc039af16504caee733
张志广 <zhange.zzg@gmail.com>
2015-07-06 18:08:37 +0800

    Fix start urls.

--- NewsCrawler/spiders/ListSpiderJandan.py
+++ NewsCrawler/spiders/ListSpiderJandan.py
@@ -34,14 +34,14 @@ class ListSpiderJandan(Spider):
     # 大丈夫：http://i.jandan.net/tag/%E5%A4%A7%E4%B8%88%E5%A4%AB
     # tech：http://i.jandan.net/tag/tech
     # 创意产品：http://i.jandan.net/tag/%E5%88%9B%E6%84%8F%E4%BA%A7%E5%93%81
-    # 走进科学：http://jandan.net/tag/%E8%B5%B0%E8%BF%9B%E7%A7%91%E5%AD%A6
+    # 走进科学：http://i.jandan.net/tag/%E8%B5%B0%E8%BF%9B%E7%A7%91%E5%AD%A6
     # start_urls = [
     #     'http://i.jandan.net/tag/%E9%87%8D%E5%8F%A3%E5%91%B3',
     #     'http://i.jandan.net/tag/%E7%AC%A8%E8%B4%BC',
     #     'http://i.jandan.net/tag/%E5%A4%A7%E4%B8%88%E5%A4%AB',
     #     'http://i.jandan.net/tag/tech',
     #     'http://i.jandan.net/tag/%E5%88%9B%E6%84%8F%E4%BA%A7%E5%93%81',
-    #     'http://jandan.net/tag/%E8%B5%B0%E8%BF%9B%E7%A7%91%E5%AD%A6',
+    #     'http://i.jandan.net/tag/%E8%B5%B0%E8%BF%9B%E7%A7%91%E5%AD%A6',
     # ]
 
     def start_requests(self):
