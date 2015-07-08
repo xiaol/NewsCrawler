65b1bdef6713c9a45598eb2b660fac1e5b400bbc
张志广 <zhange.zzg@gmail.com>
2015-07-08 15:00:17 +0800

    Fix the parser of hexun .

--- Extractors/Parse_hexun.py
+++ Extractors/Parse_hexun.py
@@ -119,7 +119,7 @@ class Parser(object):
 
             # image frame
             if child.tag == 'img':
-                img = child.get('src') or child.get('alt_src')
+                img = child.get('original') or child.get('src')
                 if img and img not in dedupe:
                     item[str(len(contents))]['img'] = img
                     contents.append(item)
