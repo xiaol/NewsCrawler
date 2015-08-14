fed6ecfa61ea66f2ccd8697b47195d01ae860ae0
zhange <zhange.zzg@gmail.com>
2015-08-14 14:18:04 +0800

    PEP8

--- Controller/spider_status.py
+++ Controller/spider_status.py
@@ -13,10 +13,12 @@ for project in list_projects:
     print '.'*60
     list_spiders = scrapyd.list_spiders(project)
     print 'project:', project
+    print 'totals: ', len(list_spiders)
     for spider in list_spiders:
         print 'spider:', spider
     status = scrapyd.list_jobs(project)
     running_spider = status['running']
+    print 'runs: ', len(running_spider)
     print 'running spider:'
     for spider in running_spider:
         print spider
